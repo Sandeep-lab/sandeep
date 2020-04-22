053212431fe84eb04cdee6c0dc16febf6a4e0a00
spalger <spalger@users.noreply.github.com>
2020-04-22 15:17:08 -0700

    create builds in ci-stats service as builds are running

--- test/scripts/jenkins_unit.sh
+++ test/scripts/jenkins_unit.sh
@@ -1,16 +1,3 @@
 #!/usr/bin/env bash
 
 source test/scripts/jenkins_test_setup.sh
-
-if [[ -z "$CODE_COVERAGE" ]] ; then
-  "$(FORCE_COLOR=0 yarn bin)/grunt" jenkins:unit --dev;
-else
-  echo " -> Running jest tests with coverage"
-  node scripts/jest --ci --verbose --coverage
-  echo ""
-  echo ""
-  echo " -> Running mocha tests with coverage"
-  yarn run grunt "test:mochaCoverage";
-  echo ""
-  echo ""
-fi
