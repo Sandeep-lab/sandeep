ca4511e5093fa52f66b64c1b3ce1510c6e60dd8b
spalger <spalger@users.noreply.github.com>
2020-04-23 15:50:30 -0700

    run kbn/optimizer early on

--- test/scripts/jenkins_build_kibana.sh
+++ test/scripts/jenkins_build_kibana.sh
@@ -6,16 +6,17 @@ echo " -> building kibana platform plugins"
 node scripts/build_kibana_platform_plugins \
   --oss \
   --scan-dir "$KIBANA_DIR/test/plugin_functional/plugins" \
+  --report-stats "some name" \
   --verbose;
 
-# doesn't persist, also set in kibanaPipeline.groovy
-export KBN_NP_PLUGINS_BUILT=true
+# # doesn't persist, also set in kibanaPipeline.groovy
+# export KBN_NP_PLUGINS_BUILT=true
 
-echo " -> Ensuring all functional tests are in a ciGroup"
-yarn run grunt functionalTests:ensureAllTestsInCiGroup;
+# echo " -> Ensuring all functional tests are in a ciGroup"
+# yarn run grunt functionalTests:ensureAllTestsInCiGroup;
 
-# Do not build kibana for code coverage run
-if [[ -z "$CODE_COVERAGE" ]] ; then
-  echo " -> building and extracting OSS Kibana distributable for use in functional tests"
-  node scripts/build --debug --oss
-fi
+# # Do not build kibana for code coverage run
+# if [[ -z "$CODE_COVERAGE" ]] ; then
+#   echo " -> building and extracting OSS Kibana distributable for use in functional tests"
+#   node scripts/build --debug --oss
+# fi
